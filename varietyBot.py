eed879d1cdfb490dd1e9d7f331d496682c1ed493
Kaushik Paul <kaushikpaul50@gmail.com>
2021-01-06 15:10:44 -0500

    Added flip function

--- varietyBot.py
+++ varietyBot.py
@@ -1,5 +1,6 @@
 import json
 import discord
+import random
 from discord.ext import commands
 
 with open('botToken.json',"r") as f:
@@ -25,6 +26,17 @@ async def poll(context,*question):
     await message.add_reaction("✅")
     await message.add_reaction("❌")
 
+@client.command(name = "flip")
+async def flip(context):
+    coin = random.randint(1,2)
+    if coin ==1:
+        result = ("Heads")
+    if coin ==2:
+        result = ("Tails")
+    myEmbed = discord.Embed(title="Coin Toss",description = "Heads or Tails", color=0x76D7C4)
+    myEmbed.add_field(name="Results:", value=result, inline=True)
+    message = await context.message.channel.send(embed=myEmbed)
+    
 @client.event
 async def on_ready():
     print("Bot is online.")
